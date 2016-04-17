e637f4484788991a890edc3390549c8111272ed4
Daniel Mason <daniel@danielmason.com>
2016-04-17 11:30:08 +0000

    Code - Added entry point

---
+++ public/index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * index.php
+ * @author    Daniel Mason <daniel@ayeayeapi.com>
+ * @copyright (c) 2016 Daniel Mason <daniel@ayeayeapi.com>
+ * @license   MIT
+ * @see       https://github.com/AyeAyeApi/tutorial-auth
+ */
+
+require_once '../vendor/autoload.php';
+
+use AyeAye\Auth\Version1;
+use AyeAye\Api\Api;
+
+$initialController = new Version1();
+$api = new Api($initialController);
+$api->go()->respond();
