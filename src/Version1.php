9b80f224805318cb0972a3684fd9b5a759688e22
Daniel Mason <daniel@danielmason.com>
2016-04-17 10:59:03 +0000

    Code - Added initial controller

---
+++ src/Version1.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Version1.php
+ * @author    Daniel Mason <daniel@ayeayeapi.com>
+ * @copyright (c) 2016 Daniel Mason <daniel@ayeayeapi.com>
+ * @license   MIT
+ * @see       https://github.com/AyeAyeApi/tutorial-auth
+ */
+
+namespace AyeAye\Auth;
+
+use AyeAye\Api\Controller;
+
+/**
+ * Class Version1
+ * @package AyeAye\Auth
+ */
+class Version1 extends Controller
+{
+
+}
